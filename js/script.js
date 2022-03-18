ab7a17f6a37f4df4f9e770b52fb281aeba480c08
rowl <airowl.gasga@gmail.com>
2022-03-18 16:41:08 +0100

    milestone 1 creato elementi lista per i todo e inserito condizione per il bar

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@
 const app = new Vue({
     el: '#app',
     data: {
-
+        todoList: [
+            {
+                text: 'Ciao sono un task',
+                done: true
+            },
+            {
+                text: 'sono il second task',
+                done: false
+            }
+        ]
     },
     methods: {
 
